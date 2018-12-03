bc8df0fd7fa63ddddd1dd8f0abeb44eaebcfb3ae
eclipse_user <eclipse_user@qq.com>
2018-12-03 21:10:16 +0800

    eeee

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class Apple {
 	
 	public static void main(String[] args){
-		System.out.println("TestEclipsegit");
+		System.out.println("TestEclipsegitbbb");
 	}
 
 }
