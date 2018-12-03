9c2af8113c3b8ccddb330f8c6833f751f922ddcc
lizihao_global <18843114887@163.com>
2018-12-03 21:39:23 +0800

    hot fix...

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -2,8 +2,9 @@ package com.atguigu.git;
 
 public class Apple {
 	public static void main(String[] args){
-		System.out.println("Tesaaatgit");		System.out.println("TestEclipsegit");
-				System.out.println("TestEclipsegit");
+		System.out.println("Tesaaatgit");
+		System.out.println("hot fix...");
+				
 		
 	}
 }
